ea866d7784689df81dd55be1d1b8cdd46085ac4f
Trevor Durán <trevclev0@gmail.com>
2024-01-03 15:48:19 -0700

    feat(build): add bun to run build

---
+++ index.ts
@@ -0,0 +1,5 @@
+import Developer from './src/Developer';
+
+const dev = new Developer('Trevor', 'male');
+
+console.log(`Hello developer ${dev.getName()} via Bun!`);
---
+++ src/Developer.ts
@@ -0,0 +1,13 @@
+export default class Developer {
+	constructor(name, gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+
+	getName() {
+		return this.name;
+	}
+	getGender() {
+		return this.gender;
+	}
+}
